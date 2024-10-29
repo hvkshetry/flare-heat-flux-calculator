a440f31e1c18ab7591139b699eb73ad01e705689
hvkshetry <hvkshetry@gmail.com>
2024-10-29 13:48:48 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { HeatFluxCalculator } from './components/HeatFluxCalculator'
+
+function App() {
+  return (
+    <div className="max-w-4xl mx-auto">
+      <HeatFluxCalculator />
+    </div>
+  )
+}
+
+export default App
